6648240401a357f31ef3c5a17994747fd342a2bc
PRATHAM-SPS <ingawalepratham@gmail.com>
2024-06-06 15:16:44 +0530

    typo

--- community_server/index.js
+++ community_server/index.js
@@ -6,11 +6,20 @@ const userRouter = require('./routes/user');
 const Port = process.env.port || 5000;
 const app = express();
 app.use(express.json());
-const mongoDBUrl = 'mongodb+srv://pratham:neebal@cluster0.fnmcpip.mongodb.net/Neebal?retryWrites=true&w=majority&appName=Cluster0';
-mongoose.connect(mongoDBUrl, { useNewUrlParser: true})
-  .then(() => console.log('MongoDB connected...'))
-  .catch(err => console.log(err));
+const mongoDBUrl = "mongodb://localhost:27017/neebal";
 
+const connectDB = async () => {
+  try {
+    await mongoose.connect(mongoDBUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log('MongoDB connected...');
+  } catch (err) {
+    console.error(err.message);
+    // Exit process with failure
+    process.exit(1);
+  }
+};
+
+connectDB();
 
 
 app.route("/").get((req,res) => res.json("your first rest api"));
