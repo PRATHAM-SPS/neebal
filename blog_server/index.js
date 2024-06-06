a0a9b937767596541da6a55ead3dc70aa0ba5667
PRATHAM-SPS <ingawalepratham@gmail.com>
2024-06-06 12:19:05 +0530

    deployement on server

---
+++ blog_server/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose =require('mongoose');
+const userRouter = require('./routes/user');
+
+
+const Port = process.env.port || 5000;
+const app = express();
+app.use(express.json());
+const mongoDBUrl = 'mongodb+srv://pratham:neebal@cluster0.fnmcpip.mongodb.net/Neebal?retryWrites=true&w=majority&appName=Cluster0';
+mongoose.connect(mongoDBUrl, { useNewUrlParser: true})
+  .then(() => console.log('MongoDB connected...'))
+  .catch(err => console.log(err));
+
+
+
+app.route("/").get((req,res) => res.json("your first rest api"));
+app.use("/user", userRouter);
+
+app.listen(Port, () => console.log('Server is running on port ${Port}'));
---
+++ blog_server/routes/user.js
@@ -0,0 +1,106 @@
+const express = require("express");
+const User = require('../models/users.models');
+const config = require("../config");
+const jwt = require("jsonwebtoken");
+const { checkToken } = require("../middleware");
+
+
+const router = express.Router();
+
+router.route('/:username').get(checkToken, async (req, res) => {
+    try{
+    const a = await User.find({username : req.params.username})
+    if(!a){
+        return res.status(404).json({msg: "User not found"});
+    }
+    const msg = {
+        data : a,
+        username: req.params.username,
+    };
+    return res.json(msg);
+}
+    catch(err){
+        res.status(400).json(err.message)
+    }
+});
+
+router.route("/login").post(async (req, res) => {
+    try {
+        const result = await User.findOne({ username: req.body.username });
+        if (!result) {
+            return res.status(403).json("Username incorrect");
+        }
+        if (result.password === req.body.password) {
+            let token = jwt.sign({username: req.body.username}, config.key, {expiresIn: "24h"});
+            res.json({
+                token: token,
+                msg: "success",
+            });
+        } else {
+            res.status(403).json("password is incorrect");
+        }
+    } catch (err) {
+        return res.status(500).json({ msg: err });
+    }
+});
+
+router.route('/register').post((req, res) => {
+    console.log("register ONGOING  ");
+    const user = new User({
+        username : req.body.username,
+        password : req.body.password,
+        email : req.body.email
+
+    });
+user.save()
+    .then(() => {
+        console.log("register DONE  ");
+        res.status(200).json('User added!')})
+   
+    .catch(err => {res.status(500).json('Error: ' + err)});
+
+    });
+
+router.route("/update/:username").patch(checkToken, async (req, res)=> {
+    try{
+    const updateuser = await User.findOneAndUpdate(
+        {username : req.params.username}, 
+        {$set:{ password:  req.body.password}},
+        {new : true}
+    );
+    if(!updateuser){
+        return res.status(404).json({msg: "User not found"});
+    }
+    const msg = {
+    message : "user updated succesfully",
+    username : req.params.username
+    }
+    return res.json(msg)
+
+}
+
+catch (err){
+    res.status(500).json(err.message)
+}
+});
+  
+router.route("/delete/:username").delete(checkToken, async (req, res) => {
+    try {
+        const deleteuser = await User.findOneAndDelete(
+            { username: req.params.username });
+        if (!deleteuser){
+            return res.status(404).json({ msg: 'User not found' });
+        }
+        const msg = {
+            msg: "user Deleted",
+            username: req.params.username,
+        };
+        return res.json(msg);
+
+    } catch (err) {
+        return res.status(500).json({ msg: err.message });
+    }
+});
+
+
+module.exports = router;
