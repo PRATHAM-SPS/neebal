8d90a7e0debaa2d65bd108434b0f84235a02fc50
PRATHAM-SPS <ingawalepratham@gmail.com>
2024-06-06 13:24:49 +0530

    cool

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose =require('mongoose');
-const userRouter = require('./user');
+const userRouter = require('./blog_server/api/user');
 
 
 const Port = process.env.port || 5000;
